1509a47920f50a0b690db4554dcd0a2453332f12
popcorn245 <popcorn245@users.noreply.github.com>
2021-01-18 20:11:09 +0000

    Deploying to gh-pages from master @ e49bb0116ed4cd01f8b6a9ef4fd7a387385c3b1a 🚀

---
+++ presets.js
@@ -0,0 +1,11 @@
+define("pagination.presets", ["require", "exports"], function (require, exports) {
+    "use strict";
+    exports.__esModule = true;
+    exports["default"] = {
+        list: {
+            props: {
+                display: "grid"
+            }
+        }
+    };
+});
