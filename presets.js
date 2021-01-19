a7815188bca80cc261ba9e9be353b01db2e97ecd
popcorn245 <popcorn245@users.noreply.github.com>
2021-01-19 01:44:59 +0000

    Deploying to gh-pages from master @ 34f0cc088e7ba9172e19085d825e8c98ee42117c 🚀

--- presets.js
+++ presets.js
@@ -1,10 +1,47 @@
-define("pagination.presets", ["require", "exports"], function (require, exports) {
+define("pagination/pagination.presets", ["require", "exports"], function (require, exports) {
     "use strict";
     exports.__esModule = true;
     exports["default"] = {
         list: {
             props: {
                 display: "grid"
+            }
+        }
+    };
+});
+define("select-tags/select-tags.presets", ["require", "exports"], function (require, exports) {
+    "use strict";
+    exports.__esModule = true;
+    var options = [
+        {
+            label: "Imortant",
+            value: "important"
+        },
+        {
+            label: "Bug",
+            value: "bug"
+        },
+        {
+            label: "Idea",
+            value: "idea"
+        }
+    ];
+    exports["default"] = {
+        "default": {
+            name: "With Options",
+            props: {
+                multiple: true,
+                options: options
+            }
+        },
+        withValue: {
+            name: "With Value",
+            props: {
+                options: options,
+                multiple: true,
+                value: [
+                    'important'
+                ]
             }
         }
     };
