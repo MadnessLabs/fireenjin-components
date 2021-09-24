1fc9b6d1545b3f16c25868133a2af5d357f4640f
Bobby Johnson <bobby@madnesslabs.net>
2021-09-24 01:27:23 -0500

    fix(): Add new map component and update dependencies

---
+++ src/components/map/map.tsx
@@ -0,0 +1,294 @@
+import { Geolocation } from "@ionic-native/geolocation";
+import { Loader } from "@googlemaps/js-api-loader"
+import {
+  Component,
+  ComponentInterface,
+  Element,
+  Event,
+  EventEmitter,
+  Host,
+  State,
+  Prop,
+  Method,
+  h,
+  Build,
+  Watch,
+} from "@stencil/core";
+
+@Component({
+  tag: "fireenjin-map",
+  styleUrl: "map.css",
+})
+export class Map implements ComponentInterface {
+  /**
+   * The Google Maps instance
+   */
+  map: google.maps.Map;
+  /**
+   * The current list of map markers
+   */
+  mapMarkers: google.maps.Marker[] = [];
+
+  @Element() mapEl: HTMLElement;
+  /**
+   * When a marker on the map is clicked
+   */
+  @Event() floodteamMapMarkerClick: EventEmitter<{
+    marker: google.maps.Marker;
+    location: {
+      position: {
+        lat: number;
+        lng: number;
+      };
+      name: string;
+      icon: string;
+      payload?: any;
+    };
+  }>;
+
+  /**
+   * The Google Maps API Key
+   */
+  @Prop() apiKey: string;
+  /**
+   * Google Maps options
+   */
+  @Prop() optins: any = {};
+  /**
+   * Should the map be visible?
+   */
+  @Prop() visible = true;
+  /**
+   * A list of markers to put onto the map
+   */
+  @Prop({
+    mutable: true,
+  })
+  markers: {
+    position: {
+      lat: number;
+      lng: number;
+    };
+    name: string;
+    icon: string;
+    payload?: any;
+  }[] = [];
+
+  /**
+   * The approximate lat and lng of the current device
+   */
+  @State()
+  position: any;
+  /**
+   * Is the map currently visible?
+   */
+  @State()
+  isVisible = true;
+
+  /**
+   * Add a marker to the map
+   * @param location The location information for the marker on the map
+   */
+  @Method()
+  async addMarker(location: {
+    position: {
+      lat: number;
+      lng: number;
+    };
+    name: string;
+    icon: string;
+    payload?: any;
+  }) {
+    const marker =
+      typeof location === "string" ? JSON.parse(location) : location;
+    const mapMarker = new google.maps.Marker({
+      position: marker.position,
+      map: this.map,
+      title: marker.name,
+      icon: {
+        url: marker.icon,
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(15, 15),
+        scaledSize: new google.maps.Size(34, 34),
+        shape: { coords: [17, 17, 18], type: "circle" },
+        optimized: false,
+      } as any,
+    });
+    mapMarker.addListener("click", () => {
+      this.onMarkerClick(mapMarker, marker);
+    });
+    this.mapMarkers.push(mapMarker);
+
+    return mapMarker;
+  }
+
+  /**
+   * Set the list of map markers
+   * @param markers A list of map markers
+   */
+  @Method()
+  @Watch("markers")
+  async setMarkers(
+    markers: {
+      position: {
+        lat: number;
+        lng: number;
+      };
+      name: string;
+      icon: string;
+      payload?: any;
+    }[] = [],
+    clearFirst = false
+  ) {
+    if (!clearFirst) {
+      await this.clearMarkers();
+    }
+    this.markers =
+      typeof markers === "string"
+        ? JSON.parse(markers)
+        : markers.length > 0
+          ? markers
+          : this.mapEl.getAttribute("markers")
+            ? JSON.parse(this.mapEl.getAttribute("markers"))
+            : [];
+    if (this.markers.length >= 1) {
+      this.markers.map(this.addMarker.bind(this));
+    }
+    return this.markers;
+  }
+
+  @Method()
+  async setZoom(level: number) {
+    return this.map.setZoom(level);
+  }
+
+  @Method()
+  async setCenter(latLng: google.maps.LatLng | google.maps.LatLngLiteral) {
+    return this.map.setCenter(latLng);
+  }
+
+  /**
+   * Clear the markers off of the map
+   */
+  @Method()
+  async clearMarkers() {
+    for (let i = 0; i < this.markers.length; i++) {
+      this.mapMarkers[i].setMap(null);
+    }
+    this.mapMarkers = [];
+
+    return true;
+  }
+
+  /**
+   * When a marker is clicked set the zoom level,
+   * animate to center, and emit an event
+   * @param marker The map marker that has been clicked
+   * @param location The location information tied to the map marker
+   */
+  onMarkerClick(
+    marker: google.maps.Marker,
+    location: {
+      position: {
+        lat: number;
+        lng: number;
+      };
+      name: string;
+      icon: string;
+      payload?: any;
+    }
+  ) {
+    this.map.setZoom(12);
+    this.map.setCenter(marker.getPosition());
+    this.floodteamMapMarkerClick.emit({
+      marker,
+      location,
+    });
+  }
+
+  /**
+   * Get a device's location via Geolocation api and
+   * fallback to location based on IP
+   * @param callback The function to run when the location is determined
+   */
+  getLocationCoords(
+    callback: (coords: { latitude: number; longitude: number } | false) => any
+  ) {
+    try {
+      Geolocation.getCurrentPosition()
+        .then((position) => {
+          if (callback && typeof callback === "function") {
+            callback(position.coords);
+          }
+        })
+        .catch(async (_error) => {
+          fetch("http://ip-api.com/json").then(async (response) => {
+            const ISPInfo = await response.json();
+            callback({
+              latitude: ISPInfo.lat,
+              longitude: ISPInfo.lon,
+            });
+          });
+          if (callback && typeof callback === "function") {
+            callback(false);
+          }
+        });
+    } catch (error) {
+      callback(false);
+    }
+  }
+
+  /**
+   * Create the instance of Google Maps
+   * @param position The latitude and longitude to center the map on
+   */
+  createMap(position: { latitude: number; longitude: number }) {
+    this.map = new google.maps.Map(this.mapEl.querySelector("#map"), {
+      zoom: 9,
+      center: {
+        lat: position.latitude,
+        lng: position.longitude,
+      },
+    });
+  }
+
+  async loadGoogleMaps() {
+    return new Promise((resolve, reject) => {
+      try {
+        const loader = new Loader({
+          apiKey: this.apiKey,
+          version: "weekly",
+          ...this.optins,
+        });
+
+        loader.load().then(() => {
+          resolve({});
+        });
+      } catch (error) {
+        reject(error)
+      }
+    });
+  }
+
+  componentDidLoad() {
+    if (Build.isBrowser) {
+      this.isVisible = this.visible;
+      this.getLocationCoords(async (coords) => {
+        this.position = coords
+          ? coords
+          : { latitude: 38.6270025, longitude: -90.19940419999999 };
+        if (!window?.google?.maps && this.apiKey) await this.loadGoogleMaps();
+        this.createMap(this.position);
+        await this.setMarkers(this.markers);
+      });
+    }
+  }
+
+  render() {
+    return (
+      <Host class={{ "map-is-visible": this.isVisible }}>
+        <div id="map" />
+      </Host>
+    );
+  }
+}
---
+++ src/components/map/readme.md
@@ -0,0 +1,78 @@
+# trackmygiving-map
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property  | Attribute | Description                           | Type                                                                                        | Default |
+| --------- | --------- | ------------------------------------- | ------------------------------------------------------------------------------------------- | ------- |
+| `markers` | --        | A list of markers to put onto the map | `{ position: { lat: number; lng: number; }; name: string; icon: string; payload?: any; }[]` | `[]`    |
+| `visible` | `visible` | Should the map be visible?            | `boolean`                                                                                   | `true`  |
+
+
+## Events
+
+| Event                     | Description                         | Type                                                                                                                                  |
+| ------------------------- | ----------------------------------- | ------------------------------------------------------------------------------------------------------------------------------------- |
+| `floodteamMapMarkerClick` | When a marker on the map is clicked | `CustomEvent<{ marker: Marker; location: { position: { lat: number; lng: number; }; name: string; icon: string; payload?: any; }; }>` |
+
+
+## Methods
+
+### `addMarker(location: { position: { lat: number; lng: number; }; name: string; icon: string; payload?: any; }) => Promise<google.maps.Marker>`
+
+Add a marker to the map
+
+#### Returns
+
+Type: `Promise<Marker>`
+
+
+
+### `clearMarkers() => Promise<boolean>`
+
+Clear the markers off of the map
+
+#### Returns
+
+Type: `Promise<boolean>`
+
+
+
+### `setCenter(latLng: google.maps.LatLng | google.maps.LatLngLiteral) => Promise<void>`
+
+
+
+#### Returns
+
+Type: `Promise<void>`
+
+
+
+### `setMarkers(markers?: { position: { lat: number; lng: number; }; name: string; icon: string; payload?: any; }[]) => Promise<any>`
+
+Set the list of map markers
+
+#### Returns
+
+Type: `Promise<any>`
+
+
+
+### `setZoom(level: number) => Promise<void>`
+
+
+
+#### Returns
+
+Type: `Promise<void>`
+
+
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
